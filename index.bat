b27a6facb6c0db78ac1486470187d2cb2d623a03
byzocker <byzocker@users.noreply.github.com>
2020-08-26 16:51:50 +0200

    Beta v0.4.2
    HAHSGD=IHASODHAISHDUIAhsfiuhawiuthiusahfuiahtuiashfuiheasuithasuifhiuthuiashfuzihtiuashcfuihtiuashdfuiwehtriuzzeehtiuaschgiuethiuaschiuhtiuashciuhtiuanshifuhiuthaiushgiuthw
    
    Pleasdse end this

---
+++ index.bat
@@ -0,0 +1,82 @@
+<!-- 
+@echo off
+mshta.exe "%~f0"
+exit /b %errorlevel%
+-->
+
+<html lang="en">
+    <head>
+      
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Startseite - ByZocker.tk</title>
+        <link rel="stylesheet" href="./css/index.css">
+    </head>
+
+    <script src="/scripts/index.js"></script>
+    <script language='javascript' type="text/javascript">
+    </script>
+
+
+    <header class="header">
+
+        <nav class="nav-items">
+           
+            <div class="navitems">
+                <a href="#about" class="about-text">Über mich</a>
+                <a href="./contact/mailme.html" class="contact-me">Kontakt</a>
+                <a href="https://www.youtube.com/channel/UCSSKp7WqHrLnJH7Vf65t0Mg?view_as=subscriber" class="ytlink">YouTube</a>
+                <a href="https://twitter.com/ByZocker" class="tweety">Twitter</a>
+            </div>
+            
+        </nav>
+
+    </header>
+
+    <h4 class="welcum">
+        Willkommen!
+    </h4>
+
+    <h5 class="lookm8">
+
+        <!Achtung!<br>
+        <!Diese Website ist noch nicht Fertig!<br>
+        <!Sowie in der Beta.
+
+    </h5>
+
+    <body>
+
+        <div class="about">
+
+            Willkommen auf meiner Website,<br>
+            Hier findest du Hauptsächlich:<br><br> <div class="about2"><b>·</b> Sachen zu meinem
+            zukünftigem Minecraft Server<br>
+            <b>·</b> Sachen zu meinem YouTube/Twitter<br> 
+            <b>·</b> Sowie dinge zu meinem Discord  </div><br>
+            Es kann sogar sein,
+            dass ich Video Links
+            hier früher poste!<br>
+            Viel Glück!
+
+        </div>
+            <br><br><br><br><br><br><br><br><br><br>
+    </body>
+
+    <footer class="foot">
+
+        <div class="medialinks">
+
+           <a href="https://discord.gg/X2nphZJ" class="dc-1"> Mein Discord</a>
+
+        </div>
+
+        <div class="cpgrht">
+            <a href="https://byzocker.tk/copyright.html" style="text-decoration: none;">©Copyright</a> <a href="https://github.com/byzocker/byzocker.github.io" style="text-decoration: none;">ByZocker.tk</a> 2020
+        </div>
+        
+
+
+    </footer>
+
+</html>
